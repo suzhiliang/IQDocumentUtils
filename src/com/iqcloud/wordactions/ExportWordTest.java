677451a09a97d53b7ea804bd25b5b48c333b7123
suzhiliang <645058999@qq.com>
2017-10-24 13:15:12 +0800

    FreeMarkerWordUtil.class

--- src/com/iqcloud/wordactions/ExportWordTest.java
+++ src/com/iqcloud/wordactions/ExportWordTest.java
@@ -1,12 +1,18 @@
 package com.iqcloud.wordactions;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import sun.misc.BASE64Encoder;
 
 /**
  * Created by blinkfox on 15-11-19.
@@ -34,8 +40,64 @@ public class ExportWordTest {
 			list.add(map);
 		}
 		testMap.put("datas", list);
-
+		List<Map<String, String>> imageDates = new ArrayList<Map<String, String>>();
+		Map<String, String> imageDate = null;
+		String imagePath = "E:/zlsu/Document/marking/framktodoc/image/";
+		Date t1 = new Date();
+		String base64 = null;
+		StringBuffer stringBuffer = new StringBuffer();
+		StringBuffer stringBuffer2 = new StringBuffer();
+		String relationship = null;
+		String relationship2 = null;
+		Integer themeNum = null;
+		List<String> rIds = new ArrayList<String>();
+		for (int i = 1; i <= 6; i++) {
+			imageDate = new HashMap<String, String>();
+			Date t2 = new Date();
+			imageDate.put("name", "/word/media/image" + i + ".jpeg");
+			base64 = imageToBase64(imagePath + i + ".jpg");
+			System.out.println("转换的base64-----------》" + base64);
+			imageDate.put("val", base64);
+			System.out.println("转换第" + i + "张图片用时----------->" + (new Date().getTime() - t2.getTime()));
+			imageDates.add(imageDate);
+			themeNum = i + 6;
+			relationship = "<Relationship Id=\"rId" + themeNum
+					+ "\"  Type=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument\" Target=\"word/document.xml\" />";
+			stringBuffer.append(relationship);
+			relationship2 = "<Relationship Id=\"rId" + themeNum
+					+ "\"  Type=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships/image\" Target=\"media/image"
+					+ i + ".jpeg\" />";
+			stringBuffer2.append(relationship2);
+			rIds.add("rId" + themeNum.toString());
+		}
+		System.out.println("转换6张图片用时----------->" + (new Date().getTime() - t1.getTime()));
+		testMap.put("relationship", stringBuffer);
+		testMap.put("relationship2", stringBuffer2);
+		testMap.put("rIds", rIds);
+		testMap.put("imageDates", imageDates);
 		return testMap;
+	}
+
+	/**
+	 * @Title: imageToBase64
+	 * @Description: 图片转换base64
+	 * @param imagePath
+	 * @return: void
+	 */
+	private static String imageToBase64(String imagePath) {
+		BASE64Encoder base64Encoder = new BASE64Encoder();
+		InputStream in = null;
+		byte[] date = null;
+		try {
+			in = new FileInputStream(new File(imagePath));
+			date = new byte[in.available()];
+			in.read(date);
+			in.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return base64Encoder.encode(date);
+
 	}
 
 	/**
@@ -43,7 +105,7 @@ public class ExportWordTest {
 	 */
 	public static void main(String[] args) {
 		Map<String, Object> testMap = createDatas();
-		WordUtil handler = new WordUtil();
+		FreeMarkerWordUtil handler = new FreeMarkerWordUtil();
 		Writer out = null;
 		try {
 			// 生成test.doc的word文件到某文件路径下
